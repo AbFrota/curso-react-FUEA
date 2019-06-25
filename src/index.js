f2e36edf83a6ff12135eacc343f2e4e18cf816ac
Ana <beatrizffrotaa@gmail.com>
2019-06-25 09:25:43 -0400

    curso de react - FUEA

--- src/index.js
+++ src/index.js
@@ -4,8 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
+//const nome = "Bia";
+//const elemento  = <h1> Olá {nome}</h1>
+
+
+ReactDOM.render(<App/>, document.getElementById('root'));   
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
